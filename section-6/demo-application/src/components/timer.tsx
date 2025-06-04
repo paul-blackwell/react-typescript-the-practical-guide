ac6a0ea2ee20e20ccd3467d9577d82033abbcbb0
paul-blackwell <paul@paulblackwell.fr>
2025-06-04 06:55:10 +0100

    Section 6: Side Effects, useEffect() & Data Fetching with TypeScript
    Actions:
    
    + 75. Creating a First Side Effect
    + 76. Using useEffect() with TypeScript
    + 77. Managing An Interval With Refs & The Effect Cleanup Function

--- section-6/demo-application/src/components/timer.tsx
+++ section-6/demo-application/src/components/timer.tsx
@@ -1,11 +1,34 @@
+import { useState, useEffect, useRef } from 'react';
+
 import { type Timer as TimerProps } from '../store/timers-context.tsx';
 import Container from './UI/container.tsx';
 
 export default function Timer({ name, duration }: TimerProps) {
+  const interval = useRef<number | null>(null);
+  const [remainingTime, setRemainingTime] = useState(duration * 1000); // covert duration to milliseconds
+
+  if (remainingTime <= 0 && interval.current) {
+    clearInterval(interval.current);
+  }
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setRemainingTime((prevTime) => prevTime - 50);
+    }, 50);
+    interval.current = timer;
+
+    return () => clearInterval(timer);
+  }, []);
+
+  const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+
   return (
     <Container as="article">
       <h2>TODO: {name}</h2>
-      <p>{duration}</p>
+      <p>
+        <progress max={duration * 1000} value={remainingTime} />
+      </p>
+      <p>{formattedRemainingTime}</p>
     </Container>
   );
 }
